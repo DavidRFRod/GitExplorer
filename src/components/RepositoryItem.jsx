54c15ffecc4334e357fa41c7c2a19ffdd63f6a7b
David Rodrigues <davidrapha@hotmail.com>
2022-05-03 10:45:17 -0300

    first commit

---
+++ src/components/RepositoryItem.jsx
@@ -0,0 +1,11 @@
+export function RepositoryItem(props) {
+    return (
+        <li>
+                    <strong>{props.title}</strong>
+                    <p>Forms in React</p>
+                    <a href="">
+                        Access on repository
+                    </a>
+               </li>
+    );
+}
---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,17 @@
+import { RepositoryItem } from "./RepositoryItem"
+
+
+export function RepositoryList(){
+    return(
+        <section className="repository-list">
+            <h1>Repository List</h1>
+            <ul>
+                <RepositoryItem title="Repository One" />
+                <RepositoryItem title="Repository Two" />
+                <RepositoryItem title="Repository One" />
+                <RepositoryItem title="Repository One" />
+                <RepositoryItem title="Repository One" />
+            </ul>
+        </section>
+    )
+}
